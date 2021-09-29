1a8176768acaefab0e4e62e9c2c9a4622934bfd1
Leonid Romanovsky <sirLeonidRomanovsky@gmail.com>
2021-09-29 18:29:05 +0300

    splatted SoftwareSerial baudrate being interrupted related bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <SoftwareSerial.h>
 #include "keys.h"
 
-#define BAUD_RATE    115200
-#define A9_BAUD_RATE 115200
+#define BAUD_RATE          115200
+#define A9G_BAUD_RATE      115200
+#define A9G_BAUD_RATE_SLOW 9600
 
 #define A9G_RX   14
 #define A9G_TX   12
@@ -12,10 +13,28 @@
 #define A9G_LOWP 2  // ESP12 GPIO2 A9/A9G ENTER LOW POWER MODULE
 
 SoftwareSerial A9G(A9G_RX, A9G_TX);
+char BUFFER2k[2048];
+char CHBUFF[2] = { 0, 0 };
 
-bool includes(String& str, String find) {
-  uint16_t len = str.length();
-  uint16_t findlen = find.length();
+void strchcat(char* str, char ch) {
+  CHBUFF[0] = ch;
+  CHBUFF[1] = 0;
+  strncat(str, CHBUFF, 1);
+}
+
+bool includes(const char* str, const char* find) {
+  /*
+  Serial.println("<includes>");
+  Serial.println("<str>");
+  Serial.println(str);
+  Serial.println("</str>");
+  Serial.println("<find>");
+  Serial.println(find);
+  Serial.println("</find>");
+  Serial.println("</includes>");
+  */
+  uint16_t len = strlen(str);
+  uint16_t findlen = strlen(find);
   if (findlen > len)
     return false;
   for (uint16_t i = 0, found = 0; i < len; i++) {
@@ -29,15 +48,16 @@ bool includes(String& str, String find) {
   return false;
 }
 
-bool await(String str, uint16_t timeout = 5000, uint16_t ttl = 1000) {
-  String buffer = "";
+bool await(const char* str, uint16_t timeout = 5000, uint16_t ttl = 1000) {
+  auto& buffer = BUFFER2k;
+  memset(buffer, 0, sizeof buffer);
   uint64_t untill = millis() + timeout + ttl;
   while (millis() <= untill) {
     if (A9G.available()) {
       while (A9G.available()) {
         char ch = A9G.read();
         Serial.write(ch);
-        buffer.concat(ch);
+        strchcat(buffer, ch);
         untill += ttl;
       }
       if (includes(buffer, str))
@@ -48,56 +68,56 @@ bool await(String str, uint16_t timeout = 5000, uint16_t ttl = 1000) {
 }
 
 bool command(
-  String str,
+  const char* str,
   uint16_t timeout = 5000,
   uint16_t ttl = 1000,
-  String success = "",
-  String error = ""
+  const char* success = "",
+  const char* error = ""
 ) {
-  A9G.println(str);
-  String buffer = "";
+  while (A9G.available())
+    Serial.write(A9G.read());
+  auto& buffer = BUFFER2k;
+  memset(buffer, 0, sizeof buffer);
+  A9G.write(str);
+  A9G.write("\r\n");
   uint64_t untill = millis() + timeout + ttl;
-  bool gotself = false;
   while (millis() <= untill) {
     if (A9G.available()) {
       while (A9G.available()) {
         char ch = A9G.read();
         Serial.write(ch);
-        buffer.concat(ch);
+        strchcat(buffer, ch);
         untill += ttl;
       }
-      if (!gotself) {
-        if (includes(buffer, str)) {
-          gotself = true;
-        } else {
-          A9G.println(str);
-          buffer = "";
-          untill = millis() + timeout + ttl;
-        };
-      } else {
-        if (
-          includes(buffer, F("+CME ERROR"))
-          || (error.length() && includes(buffer, error))
-        ) {
-          A9G.println(str);
-          buffer = "";
-          untill = millis() + timeout + ttl;
-        } else if (
-          includes(buffer, F("OK\r\n"))
-          && (!success.length() || includes(buffer, success))
-        ) return true;
-      }
+      if (
+        includes(buffer, "+CME ERROR")
+        || (!strlen(error) && includes(buffer, error))
+      ) {
+        A9G.write(str);
+        A9G.write("\r\n");
+        memset(buffer, 0, sizeof buffer);
+        untill = millis() + timeout + ttl;
+      } else if (
+        includes(buffer, "OK\r\n")
+        && (!strlen(success) || includes(buffer, success))
+      ) return true;
     }
   }
   return false;
 }
 
-void setup() {
-  Serial.begin(BAUD_RATE);
-  A9G.begin(A9_BAUD_RATE, SWSERIAL_8N1, 14, 12, false, 1024);
+void A9G_init_magic_chars() {
   for (char ch = ' '; ch <= 'z'; ch++)
     A9G.write(ch);
   A9G.println();
+}
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  Serial.println("Starting...");
+  A9G.begin(A9G_BAUD_RATE, SWSERIAL_8N1, A9G_RX, A9G_TX, false, 1200);
+  A9G_init_magic_chars();
+  Serial.println("Powering A9G...");
 
   pinMode(A9G_PON, OUTPUT);  // LOW LEVEL ACTIVE
   pinMode(A9G_POFF, OUTPUT); // HIGH LEVEL ACTIVE
@@ -112,27 +132,38 @@ void setup() {
   digitalWrite(A9G_PON, LOW);
   delay(3000);
   digitalWrite(A9G_PON, HIGH);
-  while (!await(F("READY\r\n")));
-  while (!command(F(R"(AT)")));
-  while (!command(F(R"(AT+CGDCONT=1,"IP","internet")")));
-  while (!command(F(R"(AT+CGATT=1)")));
-  while (!command(F(R"(AT+CGACT=1,1)")));
+  while (!await("READY\r\n"));
+  A9G.flush();
+  A9G.end();
+  A9G.begin(A9G_BAUD_RATE_SLOW, SWSERIAL_8N1, A9G_RX, A9G_TX, false, 1200);
+  Serial.println("Initializing...");
+  while (!command(R"(AT)", 1000, 100));
+  while (!command(R"(AT+CGDCONT=1,"IP","internet")"));
+  while (!command(R"(AT+CGATT=1)"));
+  while (!command(R"(AT+CGACT=1,1)"));
 }
 
 uint64_t last = 0;
 void loop() {
   if (millis() - last > 60000 || last == 0) {
-    const auto url = String(SERVICE_URL);
-    const auto telemetry = "4, 55.973589,  38.684297, 10,,0";
-    const auto mimetype = "text/plain";
-    const auto postcommand =
-      F("AT+HTTPPOST=\"")
-      + url
-      + "\",\""
-      + mimetype
-      + "\",\""
-      + telemetry
-      + "\"";
+    const auto boat_id = "4";
+    const auto lat = 55.973589;
+    const auto lon = 38.684297;
+    const auto speed = 10;
+    const auto rotation = 0;
+    const auto impact = 0;
+    char postcommand[512];
+    snprintf(postcommand, 512,
+      R"(AT+HTTPPOST="%s","%s","%s,%f,%f,%i,%i,%u")",
+      SERVICE_URL,
+      MIMETYPE,
+      boat_id,
+      lat,
+      lon,
+      speed,
+      rotation,
+      impact
+    );
     auto success = command(
       postcommand,
       20000,
@@ -140,5 +171,8 @@ void loop() {
       R"("status":"success")",
       R"("status":"error")");
     if (success) last = millis();
+  } else {
+    while (A9G.available())
+      Serial.write(A9G.read());
   }
 }
